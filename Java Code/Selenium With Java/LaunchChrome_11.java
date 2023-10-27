edaa7ec45ffdced7a0a9d6b9dac44917546fc7c4
Vinothini Vediyappan <148677041+vinothini0207@users.noreply.github.com>
2023-10-27 18:53:26 +0530

    Update LaunchChrome_11.java

--- Java Code/Selenium With Java/LaunchChrome_11.java
+++ Java Code/Selenium With Java/LaunchChrome_11.java
@@ -6,9 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchChrome_11{
 	
 	public static void main(String[]args) {
-		
+		//// launch chrome browser
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
+
+		//load google url, copy the url and paste it.
 		driver.get("https://google.com/");
 		
 	}
